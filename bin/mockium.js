e91dadf295e842ba5b7af213167b2a799df1d9f6
GonzaloCedillo <gonzalo.cedillo@sngular.team>
2019-03-25 11:56:40 +0100

    Listing features directory

---
+++ bin/mockium.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const program = require("commander");
+const resources = require("../lib/cli/resources");
+
+async function listDirectories(command) {
+    return new Promise(async (resolve, reject) => {
+        const featuresList = await resources.getFeaturesFromPath(command.featuresDirectory, ".feature.js").catch(err => console.error("[[ERROR]]", err));
+
+        return resolve(featuresList.map(featureFile => require(featureFile)));
+    });
+}
+
+function start() {
+    program
+        .option("-m, --mocks-directory [mocks]", "Mocks directory relative path")
+        .option("-f, --features-directory [features]", "Features directory relative path")
+        .parse(process.argv);
+
+    listDirectories(program);
+}
+
+start();
