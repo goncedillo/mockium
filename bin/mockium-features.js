c14f96a1c38d6b9d56ce62a01284c73e64f38323
GonzaloCedillo <goncedillo@gmail.com>
2019-03-26 22:23:36 +0100

    Asking for action and feature

--- bin/mockium-features.js
+++ bin/mockium-features.js
@@ -3,6 +3,7 @@
 const program = require("commander");
 const resources = require("../lib/cli/resources");
 const questions = require("../lib/cli/questions");
+const clearTerminal = require("../lib/cli/clear-terminal");
 
 async function listDirectories(command) {
   return new Promise(async (resolve, reject) => {
@@ -11,6 +12,47 @@ async function listDirectories(command) {
       .catch(err => console.error("[[ERROR]]", err));
 
     return resolve(featuresList.map(featureFile => require(featureFile)));
+  });
+}
+
+function featureSelected(feature, cb) {
+  clearTerminal();
+
+  console.log(`Selected feature: ${feature}`);
+
+  cb();
+}
+
+function exitApp() {
+  clearTerminal();
+
+  console.log("Bye, Mockium");
+
+  process.exit(0);
+}
+
+function goToOption(option, features, options) {
+  const actions = {
+    feature: () => askForFeature(features, options),
+    exit: () => exitApp()
+  };
+
+  actions[option].call();
+}
+
+function askForFeature(features, options) {
+  clearTerminal();
+
+  const filteredFeatures = features.map(item => item.name);
+
+  questions.askForFeature(filteredFeatures).then(response => {
+    featureSelected(response.menuFeature, () => askMenu(features, options));
+  });
+}
+
+function askMenu(features, options) {
+  questions.askMenuOptions(options).then(response => {
+    goToOption(response.menuOption, features, options);
   });
 }
 
@@ -24,12 +66,19 @@ async function start() {
     .parse(process.argv);
 
   const features = (await listDirectories(program)).filter(item => item.name);
-
-  console.log(features);
-
-  questions.askMenuOptions(features.map(item => item.name)).then(response => {
-    console.log("Response");
-  });
+  const menuOptions = [
+    {
+      name: "Change feature",
+      value: "feature"
+    },
+    {
+      name: "Exit",
+      value: "exit"
+    }
+  ];
+  // console.log(features);
+  clearTerminal();
+  askMenu(features, menuOptions);
 }
 
 start();
